a924fcd4a999bb96c36ab1d3ee8bfbe6282e0c8e
Anjali Paliwal <anjalipaliwal25@gmail.com>
2017-12-15 19:58:48 +0530

    first Commit

---
+++ app/src/main/java/com/firebaseuser/chatapp3/UserDetails.java
@@ -0,0 +1,11 @@
+package com.firebaseuser.chatapp3;
+
+/**
+ * Created by hp on 10-12-2017.
+ */
+
+public class UserDetails {
+    static String username = "";
+    static String password = "";
+    static String chatWith = "";
+}
